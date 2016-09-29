c82348dc0ef9765084ab91f21521ee2d5f928dc2
Ashay Shirwadkar <ashayshirwadkar12@gmail.com>
2016-09-29 22:19:23 +0530

    Removed dependency on cs50 - getline() function is used to take input - Cleaned up the code preserving the logic

--- caesar.c
+++ caesar.c
@@ -1,44 +1,56 @@
 #include<stdio.h>
-#include<cs50.h>
 #include<string.h>
 #include<stdlib.h>
+#include <errno.h>
 
-int main(int argc,string argv[])
-{
-int k;
+#define ALPHABETS 26
 
-if(argc==2)
+int main(int argc, char *argv[])
 {
-k=atoi(argv[1]);
-string s=GetString();
-for(int i=0,n=strlen(s);i<n;i++)
-{
-int c=(s[i]+k);
-if(s[i]>='A' && s[i]<='Z')
-{
-if(c<='Z')
-s[i]=c;
-else
-s[i]='A'+(c-65)%26;
-}
-else if(s[i]>='a' && s[i]<='z')
-{
-if(c<='z')
-s[i]=c;
-else
-s[i]='a'+(c-97)%26;
-}
-}
-for(int i=0,n=strlen(s);i<n;i++)
-printf("%c",s[i]);
-printf("\n");
-return 0;
-}
-else
-{
-printf("not as per required arguments");
-return 1;
-}
-}
+	int shift, i, rc, ascii_code = 0;
+	int arg = 0;
+	size_t nbytes = 0, bytes_read = 0;
+	char *data = NULL;
 
+	/* Check the command line arguments */
+	if (argc != 2) {
+		printf("usage: %s <key>", argv[0]);
+		rc = EINVAL;
+		goto out;
+	}
 
+	/* Get the shift/rotate number */
+	shift = atoi(argv[++arg]);
+	if (!shift) {
+		rc = EINVAL;
+		printf("Enter valid shifting number");
+		goto out;
+	}
+
+	/* Take the data to be encoded */
+	puts("Enter data:");
+	bytes_read = getline(&data, &nbytes, stdin);
+
+	/* Encryption logic for Caesar cryptographic algorithm */
+	for (i = 0; i < strlen(data); i++) {
+
+		ascii_code = (data[i] + shift);
+		if (data[i] >= 'A' && data[i] <= 'Z') {
+			if (ascii_code <= 'Z')
+				data[i] = ascii_code;
+			else
+				data[i] = 'A' + (ascii_code - 'A') % ALPHABETS;
+		}
+		else if (data[i] >= 'a' && data[i] <= 'z') {
+			if (ascii_code <= 'z')
+				data[i] = ascii_code;
+			else
+				data[i] = 'a' + (ascii_code - 'a') % ALPHABETS;
+		}
+	}
+
+	printf("%s", data);
+	free(data);
+out:
+	return rc;
+}
--- vigenere.c
+++ vigenere.c
@@ -1,53 +1,65 @@
-#include<stdio.h>
-#include<cs50.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc,string argv[])
-{
-if(argc==2)
-{
-string k=argv[1];
-int d=strlen(k);
-for(int i=0;i<d;i++)
-{
-if(!((k[i]>='a' && k[i]<='z')||(k[i]>='A' && k[i]<='Z')))
-{
-printf("invalid key\n");
-return 1;
-}
-}
+#define ALPHABETS 26
 
-string p=GetString();
-int j=0,c;
-for(int i=0;i<strlen(p);i++)
+int main(int argc, char *argv[])
 {
-int k1=k[j];
-if(k1>=97)
-c=(k1-97);
-else
-c=(k1-65);
-if(p[i]>='A' && p[i]<='Z')
-{
-p[i]='A'+((p[i]-65)+c)%26;
-j++;
-}
-else if(p[i]>='a' && p[i]<='z')
-{
-p[i]='a'+((p[i]-97)+c)%26;
-j++;
-}
+        int rc = 0, i = 0, arg = 0, j = 0, c = 0;
+        int ascii_code;
+        size_t key_len = 0;
+        size_t nbytes = 0, bytes_read = 0;
+        char *key = NULL, *data = NULL;
 
-if(j>=d)
-j=0;
-}
-for(int i=0;i<strlen(p);i++)
-printf("%c",p[i]);
-printf("\n");
-return 0;
-}
-else
-{
-printf("Number of agruments are less or more");
-return 1;
-}
+	/* Check the command line arguments */
+        if (argc != 2) {
+                printf("usage: %s <key>", argv[0]);
+                goto out;
+        }
+        key = argv[++arg];
+        key_len = strlen(key);
+
+        /* Check the key */
+        for (i = 0; i < key_len; i++) {
+                if (!((key[i] >= 'a' && key[i] <= 'z') ||
+		      (key[i] >= 'A' && key[i] <= 'Z'))) {
+                        printf("invalid key\n");
+                        rc = EINVAL;
+                        goto out;
+                }
+        }
+
+	/* Take the data to be encoded */
+        puts("Enter data:");
+        bytes_read = getline(&data, &nbytes, stdin);
+
+	/* Encryption logic for Vigenere cryptographic algorithm */
+        for(i = 0; i < strlen(data); i++) {
+
+                ascii_code = data[j];
+                if (ascii_code >= 'a')
+                        c = (ascii_code - 'a');
+                else
+                        c = (ascii_code - 'A');
+
+                if (data[i] >= 'A' && data[i] <= 'Z') {
+                        data[i] = 'A'+ ((data[i] - 'A') + c) % ALPHABETS;
+                        j++;
+                }
+                else if (data[i] >= 'a' && data[i] <= 'z') {
+                        data[i] = 'a' + ((data[i] - 'a') + c) % ALPHABETS;
+                        j++;
+                }
+
+                if (j >= key_len)
+                        j = 0;
+        }
+
+        printf("%s", data);
+        free(data);
+out:
+        return rc;
+        
 }
